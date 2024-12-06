d820c18810c8d5682f6cef7935dc9fbbf973fb43
Jancauskas <vytautas.jancauskas@dlr.de>
2024-12-06 17:07:21 +0100

    updated to create_dataset

--- src/methane_super_emitters/create_dataset.py
+++ src/methane_super_emitters/create_dataset.py
@@ -5,15 +5,8 @@ import datetime
 def check_if_inside(csv_line, lat_window, lon_window, time_window):
     if np.count_nonzero(lat_window == -1000) == (lat_window.shape[0] * lat_window.shape[1]):
         return False
+    csv_datetime = get_csv_datetime(csv_line)
     csv_line = csv_line.strip().split(',')
-    csv_year = int(csv_line[0][0:4])
-    csv_month = int(csv_line[0][4:6])
-    csv_day = int(csv_line[0][6:8])
-    csv_time = csv_line[1].split(':')
-    csv_hour = int(csv_time[0])
-    csv_minute = int(csv_time[1])
-    csv_second = int(csv_time[2])
-    csv_datetime = datetime.datetime(csv_year, csv_month, csv_day, csv_hour, csv_minute, csv_second)
     csv_lat = float(csv_line[2])
     csv_lon = float(csv_line[3])
     mask = lat_window != -1000
@@ -27,6 +20,18 @@ def check_if_inside(csv_line, lat_window, lon_window, time_window):
             (min_lat <= csv_lat <= max_lat) and
             (min_lon <= csv_lon <= max_lon))
 
+def get_csv_datetime(csv_line):
+    csv_line = csv_line.strip().split(',')
+    csv_year = int(csv_line[0][0:4])
+    csv_month = int(csv_line[0][4:6])
+    csv_day = int(csv_line[0][6:8])
+    csv_time = csv_line[1].split(':')
+    csv_hour = int(csv_time[0])
+    csv_minute = int(csv_time[1])
+    csv_second = int(csv_time[2])
+    csv_datetime = datetime.datetime(csv_year, csv_month, csv_day, csv_hour, csv_minute, csv_second)
+    return csv_datetime
+
 @click.command()
 @click.option('-i', '--input-file', help='Input CSV with super-emitter locations')
 @click.option('-m', '--matrix-file', help='Input NPZ file with methane data from TROPOMI')
@@ -38,13 +43,15 @@ def main(input_file, matrix_file):
     lat_matrix = methane_data['lat']
     lon_matrix = methane_data['lon']
     time_matrix = methane_data['time']
-    #start_date = time_matrix.min()
-    #end_date = time_matrix.max()
+    start_date = time_matrix[lat_matrix != -1000].min()
+    end_date = time_matrix[lat_matrix != -1000].max()
     rows, cols = methane_matrix.shape
     print(f"Examining {matrix_file}!")
+    for csv_line in data:
+        if not (start_date <= get_csv_datetime(csv_line) <= end_date):
+            print("SKIP!")
     for row in range(0, rows, 16):
         for col in range(0, cols, 16):
-            print(f"Examining patch ({row}, {col})")
             if row + 32 < rows and col + 32 < cols:
                 methane_window = methane_matrix[row:row + 32][:, col:col + 32]
                 lat_window = lat_matrix[row:row + 32][:, col:col + 32]
