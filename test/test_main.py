6e2c5dcf9c11018b6dd1db47d9a308a9c8573951
Vytautas Jancauskas <vytautas.jancauskas@dlr.de>
2025-01-16 11:24:32 +0100

    added a test for the model

--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,9 @@
 import pytest
+import lightning as L
+from methane_super_emitters.model import SuperEmitterDetector
 
-def test_main():
-    assert(1 + 1 == 2)
+def test_model():
+    model = SuperEmitterDetector()
+    datamodule = TROPOMISuperEmitterDataModule('./data/dataset')
+    trainer = L.trainer(gpus=0, max_epochs=1)
+    trainer.fit(model=model, datamodule=datamodule)
