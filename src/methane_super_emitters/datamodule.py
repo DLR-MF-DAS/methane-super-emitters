8b6fbc7cbc7bca4f88a7f645e8f5f39889092f78
Vytautas Jancauskas <vytautas.jancauskas@dlr.de>
2025-01-16 10:25:53 +0100

    implemented the data module

--- src/methane_super_emitters/datamodule.py
+++ src/methane_super_emitters/datamodule.py
@@ -1,26 +1,30 @@
 import lightning as L
 from torch.utils.data import random_split, DataLoader
+from methane_super_emitters.dataset import TROPOMISuperEmitterDataset
 
 class TROPOMIDataModule(L.LightningDataModule):
     def __init__(self, data_dir, batch_size=32):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
+        self.dataset = TROPOMOISuperEmitterDataset(self.data_dir)
+        self.train_set, self.val_set, self.test_set =\
+            random_split(self.dataset, [0.7, 0.15, 0.15])
 
     def setup(self, stage):
         pass
 
     def train_dataloader(self):
-        pass
+        return DataLoader(self.train_set, batch_size=self.batch_size, shuffle=True)
 
     def val_dataloader(self):
-        pass
+        return DataLoader(self.val_set, batch_size=self.batch_size, shuffle=True)
 
     def test_dataloader(self):
-        pass
+        return DataLoader(self.test_set, batch_size=self.batch_size, shuffle=True)
 
     def predict_dataloader(self):
-        pass
+        return DataLoader(self.test_set, batch_size=self.batch_size, shuffle=True)
 
     def teardown(self, stage):
         pass
