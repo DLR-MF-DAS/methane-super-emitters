dd87922a2bfef37ba63d79ed00ce8d93cf812eb1
Vytautas Jancauskas <unaudio@gmail.com>
2025-01-20 15:33:32 +0100

    reworked a visualization script

--- src/methane_super_emitters/show.py
+++ src/methane_super_emitters/show.py
@@ -1,13 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import click
+import os
+import glob
+from pathlib import Path
 
 @click.command()
-@click.option('-i', '--input-file', help='Input NPZ file')
-def main(input_file):
-    data = np.load(input_file)
-    plt.imshow(data['methane'], vmin=-30, vmax=30)
-    plt.show()
+@click.option('-i', '--input-dir', help='Input directory')
+@click.option('-o', '--output-dir', help='Output directory')
+def main(input_dir, output_dir):
+    for input_file in glob.glob(os.path.join(input_dir, '*.npz')):
+        plt.figure(figsize=(8, 8))
+        data = np.load(input_file)
+        m = data['methane']
+        m[data['mask']] = np.median(m)
+        plt.imshow(m, vmin=-30, vmax=30)
+        x = np.arange(data['methane'].shape[1])
+        y = np.arange(data['methane'].shape[0])
+        xv, yv = np.meshgrid(x, y)
+        plt.quiver(xv, yv, data['u'], data['v'])
+        stem = Path(input_file).stem
+        plt.savefig(os.path.join(output_dir, stem + ".png"))
 
 if __name__ == '__main__':
     main()
