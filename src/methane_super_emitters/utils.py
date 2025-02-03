ec7b6f7e465777fac6c8890a5e2dcd5d6b17057b
Vytautas Jancauskas <vytautas.jancauskas@dlr.de>
2025-02-03 16:11:55 +0100

    added a docstring to sample_files

--- src/methane_super_emitters/utils.py
+++ src/methane_super_emitters/utils.py
@@ -4,7 +4,16 @@ import random
 import shutil
 
 def sample_files(glob_pattern, output_dir, n):
-    """A small utility function to sample files from a directory at randm.
+    """A small utility function to sample files from a directory at random.
+
+    Parameters
+    ----------
+    glob_pattern: str
+        A string of the form /path/to/*.npz or so. To select files from.
+    output_dir: str
+        A directory name into which to copy files.
+    n: int
+        How many files to take at random.
     """
     for path in random.sample(list(glob.glob(glob_pattern)), k=n):
         shutil.copy(path, output_dir)
