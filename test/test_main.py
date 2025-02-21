cebf3578d40cfab29cd6a6bc24d17fbec1a86c78
Vytautas Jancauskas <unaudio@gmail.com>
2025-02-21 16:48:43 +0100

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -4,8 +4,8 @@ from methane_super_emitters.model import SuperEmitterDetector
 from methane_super_emitters.datamodule import TROPOMISuperEmitterDataModule
 
 def test_model():
-    model = SuperEmitterDetector()
-    datamodule = TROPOMISuperEmitterDataModule('./data/dataset_wf')
-    trainer = L.Trainer(max_epochs=1)
+    model = SuperEmitterDetector(fields=['methane', 'qa', 'u10', 'v10'])
+    datamodule = TROPOMISuperEmitterDataModule('./data/dataset', fields=['methane', 'qa', 'u10', 'v10'])
+    trainer = L.Trainer(max_epochs=10)
     trainer.fit(model=model, datamodule=datamodule)
     trainer.test(model=model, datamodule=datamodule)
