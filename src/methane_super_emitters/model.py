365c85213a3765f27b95198fa48f408d35dcdf7a
Vytautas Jancauskas <vytautas.jancauskas@dlr.de>
2025-02-24 14:45:02 +0100

    initial version of the locator model

--- src/methane_super_emitters/model.py
+++ src/methane_super_emitters/model.py
@@ -4,6 +4,50 @@ import torch
 import torch.nn as nn
 
 
+class SuperEmitterLocator(L.LightningModule):
+    def __init__(self, fields):
+        self.fields = fields
+        super().__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(len(self.fields), 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((8, 8))
+        )
+        self.fc_layers = nn.Sequential(
+            nn.Linear(128 * 8 * 8, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 32 * 32),  # Output layer
+        )
+
+    def forward(self, x):
+        out = self.conv_layers(x)
+        out = out.view(out.size(0), -1)
+        out = self.fc_layers(out)
+        out = torch.softmax(out, dim=1)
+        out = out.view(out.shape[0], 32, 32)
+        return out
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        y_hat_flat = y_hat.view(y.shape[0], -1)
+        y_indices = y.view(y.shape[0], -1).argmax(dim=1)
+        loss = torch.nn.functional.cross_entropy(y_hat_flat, y_indices)
+        self.log("train_loss", loss)
+        return loss
+
+
 class SuperEmitterDetector(L.LightningModule):
     def __init__(self, fields, dropout=0.5, weight_decay=0.01):
         self.fields = fields
--- test/test_main.py
+++ test/test_main.py
@@ -1,8 +1,14 @@
 import pytest
 import lightning as L
-from methane_super_emitters.model import SuperEmitterDetector
+from methane_super_emitters.model import SuperEmitterDetector, SuperEmitterLocator
 from methane_super_emitters.datamodule import TROPOMISuperEmitterDataModule
 from methane_super_emitters.optimize import optimize_model
+
+def test_locator():
+    model = SuperEmitterLocator(fields=['methane', 'qa', 'u10', 'v10'])
+    datamodule = TROPOMISuperEmitterDataModule('./data/dataset', fields=['methane', 'qa', 'u10', 'v10'])
+    trainer = L.Trainer(max_epochs=10)
+    trainer.fit(model=model, datamodule=datamodule)
 
 def test_model():
     model = SuperEmitterDetector(fields=['methane', 'qa', 'u10', 'v10'])
