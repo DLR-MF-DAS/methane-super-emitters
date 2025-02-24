f5f868f66de1bee67a7dd606bc038f507e7828f3
Vytautas Jancauskas <vytautas.jancauskas@dlr.de>
2025-02-24 16:40:55 +0100

    added train_locate.py

---
+++ src/methane_super_emitters/train_locate.py
@@ -0,0 +1,20 @@
+import click
+import lightning as L
+from methane_super_emitters.model import SuperEmitterLocator
+from methane_super_emitters.datamodule import TROPOMISuperEmitterDataModule
+
+
+@click.command()
+@click.option("-i", "--input-dir", help="Data directory")
+@click.option("-m", "--max-epochs", help="Maximum number of epochs", default=1)
+def train_model(input_dir, max_epochs):
+    fields = ["methane", "u10", "v10", "qa"]
+    model = SuperEmitterLocator(fields=fields)
+    datamodule = TROPOMISuperEmitterDataModule(input_dir, fields=fields)
+    trainer = L.Trainer(max_epochs=max_epochs)
+    trainer.fit(model=model, datamodule=datamodule)
+    #trainer.test(model=model, datamodule=datamodule)
+
+
+if __name__ == "__main__":
+    train_model()
